3311e44204900575a855dfa202dab5a54bbb466f
Philip Haggard <philip.r.haggard@gmail.com>
2024-09-25 16:42:32 -0500

    Add dynamic values to a component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 
 function App() {
+  const title = "Welcome to my blog!";
+  const likes = 50;
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{ title }</h1>
+        <p>Liked { likes } times</p>
+        <p>{ Math.random() * 10 }</p>
       </div>
     </div>
   );
