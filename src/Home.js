f93b8dcf0dfb27d9f675060a29a86c40d4a82591
Philip Haggard <philip.r.haggard@gmail.com>
2024-09-26 09:42:32 -0500

    Implement click events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    const handleClick = () => {
+        console.log('hello, ninjas');
+    }
+
+    const handleClickAgain = (name) => {
+        console.log('hello ' + name);
+    }
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={() => handleClickAgain('Philip')}>Click Me Again</button>
         </div>
     );
 }
